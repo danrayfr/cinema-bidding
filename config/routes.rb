cc63fd76a89a0a5c52578375c560175e8aed1e9e
danrayfr <danrayrollan98@gmail.com>
2023-03-02 18:02:34 +0800

    fix layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'dashboard', to: "dashboard#index"
     resources :cinemas
     resources :movies
     resources :showings
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-User.create!(first_name: "Test", last_name: "Admin", mobile:"09560635541",email: "test@admin.com", password: "password", password_confirmation: "password", role: User.roles[:admin])
-
 User.create!(first_name: "Dan Ray", last_name: "Rollan", mobile:"09560635541",email: "danrayrollan98@gmail.com", password: "password", password_confirmation: "password", role: User.roles[:admin])
+
+User.create!(first_name: "Test", last_name: "Admin", mobile:"09560635541",email: "test@admin.com", password: "password", password_confirmation: "password", role: User.roles[:admin])
 
 User.create!(first_name: "Test", last_name: "User", mobile:"09560635541",email: "test@user.com", password: "password", password_confirmation: "password")
