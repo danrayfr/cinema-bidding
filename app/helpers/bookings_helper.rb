06f4f5343037436a9882d8aad85cde5597b39906
danrayfr <danrayrollan98@gmail.com>
2023-03-03 14:37:55 +0800

    add fully booked update

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,19 @@
 module BookingsHelper
+
+  def check_if_booking_exists?(user_id, show_id)
+    Booking.exists?(user_id: user_id, showing_id: show_id)
+  end
+  
+  def check_if_booking_seats_is_occupied?(seat_id, showing_id)
+    Booking.exists?(seat_id: seat_id, showing_id: showing_id)
+  end
+
+  def seats_query(show_cinema_id)
+    Seat.where(cinema_id: show_cinema_id)
+  end
+
+  def remaining_seats(show_cinema_id, show_id)
+    Seat.where(cinema_id: show_cinema_id).count - Booking.where(showing_id: show_id).count
+  end
+  
 end
