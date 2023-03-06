fdff3d0d3ee14732172cdab6f784c75aac1eab90
danrayfr <danrayrollan98@gmail.com>
2023-03-06 19:08:37 +0800

    fix test

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in users(:danray)
     get admin_dashboard_url
     assert_response :success
   end
--- test/controllers/admin/showings_controller_test.rb
+++ test/controllers/admin/showings_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class Admin::ShowingsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
+    sign_in users(:danray)
     get admin_showings_url
     assert_response :success
   end
